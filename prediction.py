39fd10006c19417f4790ae7d684c3a4af078ed99
Lai-Justin <40769364+Lai-Justin@users.noreply.github.com>
2022-06-03 17:07:38 -0700

    Add files via upload

--- prediction.py
+++ prediction.py
@@ -10,31 +10,34 @@ from torchvision import datasets, transforms, models
 from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import ToTensor
 
-
-training_data = datasets.ImageFolder('hw4_train', transform = transforms.ToTensor())
-
-training_set, validation_set = data.random_split(training_data, [40320, 10080])
-
-
-
-training_loader = DataLoader(training_set, batch_size = 32, shuffle = True)
-
-
-validation_loader = DataLoader(validation_set, batch_size = 32, shuffle = True)
-
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+torch.cuda.empty_cache()
 
+training_loader = 0
+validation_loader = 0
+neural_network = 0
+
+
+#loads the  data from the training set, splits the training set into a training_loader and validation_loader. 80/20 in the data
+def load_data(training_dataset, batch_size):
+    global training_loader
+    global validation_loader
+    training_data = datasets.ImageFolder(training_dataset, transform = transforms.ToTensor())
+    training_set, validation_set = data.random_split(training_data, [40320, 10080])
+    training_loader = DataLoader(training_set, batch_size = batch_size, shuffle = True)
+    validation_loader = DataLoader(validation_set, batch_size = batch_size, shuffle = True)
+    
+
+# creating the convoluted neural network
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-
-        self.conv1 = nn.Conv2d(3, 128, 3, padding = 1)
-        self.conv2 = nn.Conv2d(128, 256, 3, padding = 1)
-        self.fc1 = nn.Linear(7 * 7 * 256, 10000)
+        self.conv1 = nn.Conv2d(3, 256, 3, padding = 1)
+        self.conv2 = nn.Conv2d(256, 512, 3, padding = 1)
+        self.fc1 = nn.Linear(7 * 7 * 512, 20000)
         self.dropout = nn.Dropout(0.4)
-        self.fc2 = nn.Linear(10000, 10)
+        self.fc2 = nn.Linear(20000, 10)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -49,119 +52,67 @@ class CNN(nn.Module):
         x = self.fc2(x)
 
         return x
-
-
-neural_network = CNN()
-neural_network = neural_network.to(device)
-loss_function = nn.CrossEntropyLoss()
-learning_rate = 0.001
-optimizer = optim.SGD(neural_network.parameters(), lr = learning_rate)
-
-epochs = 50
-
-
-
-
-
-neural_network.train()
-
-
-for i in range(epochs):
-    current_loss = 0
-    
-    for index, (images , labels) in enumerate(training_loader):
-        images, labels = images.to(device), labels.to(device)
-        optimizer.zero_grad()
-        
-
-        out = neural_network(images)
-        loss = loss_function(out, labels)
-        loss.backward()
-        optimizer.step()
-
-        current_loss += loss.item()
-
-    else:
-        print(f"Training loss: {current_loss/len(training_loader)}")
-
-neural_network.eval()
-
-    #validation
-with torch.no_grad():
-    correct = 0
-    error = 0
-    valid_loss = 0
-    for images, labels in validation_loader:
-        images, labels = images.to(device), labels.to(device)
-        predicted = neural_network(images)
-        valid_loss += loss_function(predicted, labels).item()
-        correct += (predicted.argmax(1) == labels).type(torch.float).sum().item()
-        
-        
     
 
-    print(correct / len(validation_loader.dataset))
+#trains the neural network
+def training(epochs, learning_rate, loss_function):
+    global neural_network
+    neural_network = CNN().to(device)
+    optimizer = optim.SGD(neural_network.parameters(), lr = learning_rate) 
+    neural_network.train()
+
+    for i in range(epochs):
+        current_loss = 0
+        for index, (images , labels) in enumerate(training_loader):
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            out = neural_network(images) #runs the neural network on each image
+            loss = loss_function(out, labels)
+            loss.backward() #backpropagation
+            optimizer.step()
+            current_loss += loss.item()
+
+        else:
+            print(f"Training loss: {current_loss/len(training_loader)}")
+
+#validation
+def validation(loss_function):
+    neural_network.eval()
+    with torch.no_grad():
+        correct = 0
+        error = 0
+        valid_loss = 0
+        for images, labels in validation_loader:
+            images, labels = images.to(device), labels.to(device)
+            predicted = neural_network(images)
+            valid_loss += loss_function(predicted, labels).item()
+            correct += (predicted.argmax(1) == labels).type(torch.float).sum().item()
+
+        print(correct / len(validation_loader.dataset))
 
 
-#testing
-with torch.no_grad():
-    with open("prediction.txt",'w') as f:
-        pass
-        for i in sorted(os.listdir('hw4_test'), key = len):
-            picture = os.path.join('hw4_test', i)
-            temp = Image.open(picture).convert('RGB')
-            transform = transforms.Compose([transforms.ToTensor()])
-            temp = transform(temp)
-            temp = temp.to(device)
-            temp = temp.unsqueeze(0)
-            output = neural_network(temp)
-            _, predicted = torch.max(output.data, 1)
-            predicted = predicted.cpu()
-            predicted = predicted.numpy()
-            f.write(str(predicted[0]))
-            f.write('\n')
+#testing, prints the results of the testing data into a txt file
+def testing(testing_file):
+    with torch.no_grad():
+        with open("prediction.txt",'w') as f:
+            pass
+            for i in sorted(os.listdir(testing_file), key = len):
+                picture = os.path.join(testing_file, i)
+                temp = Image.open(picture).convert('RGB')
+                transform = transforms.Compose([transforms.ToTensor()])
+                temp = transform(temp)
+                temp = temp.to(device)
+                temp = temp.unsqueeze(0)
+                output = neural_network(temp)
+                _, predicted = torch.max(output.data, 1)
+                predicted = predicted.cpu()
+                predicted = predicted.numpy()
+                f.write(str(predicted[0]))
+                f.write('\n')
 
 
 
-
-
-
-
-
-
-
-
-"""
-Implement the testing procedure here. 
-
-Inputs:
-    Unzip the hw4_test.zip and place the folder named "hw4_test" in the same directory of your "prediction.py" file, your "prediction.py" need to give the following required output.
-
-Outputs:
-    A file named "prediction.txt":
-        * The prediction file must have 10000 lines because the testing dataset has 10000 testing images.
-        * Each line is an integer prediction label (0 - 9) for the corresponding testing image.
-        * The prediction results must follow the same order of the names of testing images (0.png – 9999.png).
-    Notes: 
-        1. The teaching staff will run your "prediction.py" to obtain your "prediction.txt" after the competition ends.
-        2. The output "prediction.txt" must be the same as the final version you submitted to the CodaLab, 
-        otherwise you will be given 0 score for your hw4.
-
-
-**!!!!!!!!!!Important Notes!!!!!!!!!!**
-    To open the folder "hw4_test" or load other related files, 
-    please use open('./necessary.file') instead of open('some/randomly/local/directory/necessary.file').
-
-    For instance, in the student Jupyter's local computer, he stores the source code like:
-    - /Jupyter/Desktop/cs165B/hw4/prediction.py
-    - /Jupyter/Desktop/cs165B/hw4/hw4_test
-    If he/she use os.chdir('/Jupyter/Desktop/cs165B/hw4/hw4_test'), this will cause an IO error 
-    when the teaching staff run his code under other system environments.
-    Instead, he should use os.chdir('./hw4_test').
-
-
-    If you use your local directory, your code will report an IO error when the teaching staff run your code,
-    which will cause 0 score for your hw4.
-"""
-
-
+load_data('training_set', 32)
+training(50, 0.001, nn.CrossEntropyLoss())
+validation(nn.CrossEntropyLoss())
+testing('testing_set')
